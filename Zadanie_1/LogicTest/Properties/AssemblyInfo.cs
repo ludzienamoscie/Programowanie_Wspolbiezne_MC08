eb604e1a6c02842ec79a0291f6e47a2fb1fdce78
236527 Frydlewska Maja <236527@edu.p.lodz.pl>
2022-04-20 21:22:11 +0200

    testy funkcji matematycznych w logice

---
+++ Zadanie_1/LogicTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LogicTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LogicTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ef3064c5-6721-426d-8fec-faf690d08f4b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Zadanie_1/LogicTest/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic;
+using System;
+
+namespace LogicTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void EuclideanTest()
+        {
+            BallFactory ballFactory = new BallFactory();
+            Vector2D starting_p = new Vector2D(0, 0);
+            Vector2D end_p = new Vector2D(1, 1);
+            double actual_dist = ballFactory.EuklideanDist(starting_p, end_p);
+            Assert.AreEqual(2, actual_dist * actual_dist, 0.0001);
+        }
+        [TestMethod]
+        public void LinearFactorsTest()
+        {
+            BallFactory ballFactory = new BallFactory();
+            Vector2D point_1 = new Vector2D(0, 1);
+            Vector2D point_2 = new Vector2D(1, 1.5);
+            double a, b;
+            (a, b) = ballFactory.LinearFactors(point_1, point_2);
+            Assert.AreEqual(0.5, a, 0.0001);
+            Assert.AreEqual(1, b, 0.0001);
+        }
+        [TestMethod]
+        public void QuadRootsTest()
+        {
+            BallFactory ballFactory = new BallFactory();
+            double sol1, sol2;
+            (sol1, sol2) = ballFactory.QuadRoots(1, 3, 2);
+            Assert.AreEqual(sol1, -1, 0.0001);
+            Assert.AreEqual(sol2, -2, 0.0001);
+        }
+    }
+}
