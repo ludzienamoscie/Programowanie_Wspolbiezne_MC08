bb115daa6a384a08a077a05bc0151ce0b97c1b92
236527 Frydlewska Maja <236527@edu.p.lodz.pl>
2022-05-21 19:44:37 +0200

    Dodano logowanie zderzen

--- Zadanie_1/Logic/BallFactory.cs
+++ Zadanie_1/Logic/BallFactory.cs
@@ -28,6 +28,77 @@ namespace Logic
         private readonly object locker = new object();
         private CancellationToken _token;
         private CancellationTokenSource _tokenSource;
+        private string log_path = "ball_log.json";
+
+        private class CollisionInfo
+        {
+            Vector2D _position_ball_1;
+            Vector2D _position_ball_2;
+            Vector2D _initial_vel_1;
+            Vector2D _initial_vel_2;
+            Vector2D _final_vel_1;
+            Vector2D _final_vel_2;
+            Ball _ball_1;
+            Ball _ball_2;
+
+            public CollisionInfo(Vector2D position_ball_1,
+            Vector2D position_ball_2,
+            Vector2D initial_vel_1,
+            Vector2D initial_vel_2,
+            Vector2D final_vel_1,
+            Vector2D final_vel_2,
+            Ball ball_1,
+            Ball ball_2)
+            {
+                this._position_ball_1 = position_ball_1;
+                this._position_ball_2 = position_ball_2;
+                this._initial_vel_1 = initial_vel_1;
+                this._initial_vel_2 = initial_vel_2;
+                this._final_vel_1 = final_vel_1;
+                this._final_vel_2 = final_vel_2;
+                this._ball_1 = ball_1;
+                this._ball_2 = ball_2;
+            }
+
+            public Vector2D PositionBall1
+            {
+                get => _position_ball_1;
+            }
+            public Vector2D PositionBall2
+            {
+                get => _position_ball_2;
+            }
+
+            public Vector2D InitialVel1
+            {
+                get => _initial_vel_1;
+            }
+
+            public Vector2D InitialVel2
+            {
+                get => _initial_vel_2;
+            }
+
+            public Vector2D FinalVel1
+            {
+                get => _final_vel_1;
+            }
+
+            public Vector2D FinalVel2
+            {
+                get => _final_vel_2;
+            }
+
+            public Ball Ball_1
+            {
+                get => _ball_1;
+            }
+
+            public Ball Ball_2
+            {
+                get => _ball_2;
+            }
+        }
         public BallFactory() : this(DataAbstractAPI.CreateBallData()) { }
         public BallFactory(DataAbstractAPI data) { _data = data; }
         // Tworzenie kul
@@ -137,8 +208,18 @@ namespace Logic
             // Sekcja krytyczna
             lock (locker)
             {
+                Vector2D initVel1 = ball1.Velocity;
+                Vector2D initVel2 = ball2.Velocity;
                 ball1.Velocity = newV1;
                 ball2.Velocity = newV2;
+                CollisionInfo collisionInfo = new CollisionInfo(ball1.Position, ball2.Position, initVel1, initVel2, ball1.Velocity, ball2.Velocity, ball1, ball2);
+
+                var options = new JsonSerializerOptions { WriteIndented = true };
+                string jsonCollisionInfo = JsonSerializer.Serialize(collisionInfo, options);
+                string now = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss.fff");
+                string newJsonObject = "{" + String.Format("\n\t\"datetime\": \"{0}\",\n\t\"collision\":{1}\n", now, jsonCollisionInfo) + "}";
+
+                AppendObjectToJSONFile(log_path, newJsonObject);
             }
         }
 
@@ -156,44 +237,48 @@ namespace Logic
 
         public void logBalls(IList balls)
         {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonBalls = JsonSerializer.Serialize(balls, options);
+            string now = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss.fff");
+
+            string newJsonObject = "{" + String.Format("\n\t\"datetime\": \"{0}\",\n\t\"balls\":{1}\n", now, jsonBalls) + "}";
             lock (locker)
             {
-                var options = new JsonSerializerOptions { WriteIndented = true };
-                string jsonBalls = JsonSerializer.Serialize(balls, options);
-                string now = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss.fff");
+                AppendObjectToJSONFile(log_path, newJsonObject);
+            }
+        }
 
-                string filename = "ball_log.json";
-                string newJsonObject = "{" + String.Format("\n\t\"date\": \"{0}\",\n\t\"balls\":{1}\n", now, jsonBalls) + "}";
-                if (!File.Exists(filename))
+        private void AppendObjectToJSONFile(string filename, string newJsonObject)
+        {
+            if (!File.Exists(filename))
+            {
+                using (StreamWriter sw = new StreamWriter(filename, true))
                 {
-                    using (StreamWriter sw = new StreamWriter(filename, true))
-                    {
-                        sw.WriteLine("[]");
-                    }
+                    sw.WriteLine("[]");
                 }
+            }
 
-                string content;
-                using (StreamReader sr = File.OpenText(filename))
-                {
-                    content = sr.ReadToEnd();
-                }
-                // Jeżeli pierwszy obiekt
-                content = content.TrimEnd();
-                content = content.Remove(content.Length - 1, 1);
-                // Pierwszy obiekt, nie dodawaj przecinka przed
-                if (content.Length == 1)
-                {
-                    content = String.Format("{0}\n{1}\n]\n", content.Trim(), newJsonObject);
-                }
-                else
-                {
-                    content = String.Format("{0},\n{1}\n]\n", content.Trim(), newJsonObject);
-                }
+            string content;
+            using (StreamReader sr = File.OpenText(filename))
+            {
+                content = sr.ReadToEnd();
+            }
+            // Jeżeli pierwszy obiekt
+            content = content.TrimEnd();
+            content = content.Remove(content.Length - 1, 1);
+            // Pierwszy obiekt, nie dodawaj przecinka przed
+            if (content.Length == 1)
+            {
+                content = String.Format("{0}\n{1}\n]\n", content.Trim(), newJsonObject);
+            }
+            else
+            {
+                content = String.Format("{0},\n{1}\n]\n", content.Trim(), newJsonObject);
+            }
 
-                using (StreamWriter sw = File.CreateText(filename))
-                {
-                    sw.Write(content);
-                }
+            using (StreamWriter sw = File.CreateText(filename))
+            {
+                sw.Write(content);
             }
         }
     }
